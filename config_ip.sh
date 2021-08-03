4673931dc8f701da2c2c88d55ea9e432ea18452b
MadeleineKongshavn <Madekoro@hotmail.com>
2021-08-03 13:22:26 +0200

    adding config_ip

---
+++ config_ip.sh
@@ -0,0 +1,36 @@
+#Set the IP address for this device. 
+
+
+## 193.156.3.73/25
+## 193.156.3.74/25
+## 193.156.3.75/25
+
+
+echo "Current setup!"
+ifconfig
+
+$IP=$1
+
+echo "" > /etc/config/network
+
+echo "config interface 'loopback'"                                 >> /etc/config/network
+echo "    option ifname 'lo'"                                  >> /etc/config/network
+echo "    option proto 'static'"                               >> /etc/config/network
+echo "    option ipaddr '127.0.0.1'"                           >> /etc/config/network
+echo "    option netmask '255.0.0.0'"                          >> /etc/config/network
+echo ""                                                     >> /etc/config/network
+
+echo "config globals 'globals'"                                    >> /etc/config/network
+echo "    option ula_prefix 'fdd7:b382:9259::/48'"             >> /etc/config/network
+                                                            >> /etc/config/network
+echo "config interface 'lan'"                                      >> /etc/config/network
+echo "        option ifname 'eth0'"                                >> /etc/config/network
+echo "        option proto 'static'"                               >> /etc/config/network
+echo "        option ipaddr '193.156.3.74'"                        >> /etc/config/network
+echo "       option gateway '193.156.3.1'"                        >> /etc/config/network
+echo "        option netmask '255.255.255.0'"                      >> /etc/config/network
+echo "        option dns '129.240.2.3'"                            >> /etc/config/network
+echo "        option ip6assign '60'"                               >> /etc/config/network
+
+echo "\n\nAfter setup!"
+ifconfig
---
+++ hostapd_setup.sh
@@ -0,0 +1,49 @@
+
+
+## Changes done in /etc/config/wireless
+
+/etc/config/wireless
+
+option disabled '1'--> option disabled '0'
+
+#change to:
+config	wifi-device	'wl0'
+
+#Set interface 
+#Specifies the hardware mode, possible values are 11b for 2.4 GHz (used for legacy 11b only), 11g for 2.4 GHz (used for 802.11b, 802.11g and 802.11n) and 11a for 5 GHz (used for 802.11a, 802.11n and 802.11ac). Note that 11ng, 11na, 11n and 11ac are invalid options, this setting should largely be seen as controlling the frequency band.
+option hwmode '11a' --> option hwmode '11g'
+
+#default for openwrt RP4 is a setup with 5GH. we are changing and using 2.4GHZ
+option channel '36' -> option channel '6'
+
+#A complete wireless configuration contains at least one wifi-iface section per adapter to define a wireless network on
+#top of the hardware. Some drivers support multiple wireless networks per device. A minimal example for a wifi-iface declaration is given below.
+
+config	wifi-iface
+	option	device		'wl0'
+	option	network		'lan'
+	option	mode		'ap'
+	option	ssid		'OpenWrtAP'
+	option	encryption	'psk2'
+	option	key		'secret'
+
+    wifi up wlan2; # WiFi Up: 
+    wifi down wlan2. # WiFi down 
+
+
+
+
+#NOTE FOR 5GHZ: If you have configured 5GHz Wi-Fi and have just enabled it, but the 5 GHz Wi-Fi does not seem to start up, consider 
+#the following: If your device supports Wi-Fi channels > 100, your OpenWrt device first must scan for weather radar on these channels, before 
+#you can actually use such channels for Wi-Fi. This may take 1-10 minutes onetime after first reboot depending on your Wi-Fi situation and 
+#depending on the number of device-supported channels > 100. You may also experience 1 minute delay on each automatic channel change, as 
+#the same scan delay is required for regulation compliance.
+
+## Wireless lan confiugred!
+uci show wireless
+
+echo
+echo "----------------------------------------------"
+echo "                AP configured!                "
+echo "----------------------------------------------"
+echo
